b7a9080048c3198fba04612e66bd14bc555deb1f
somya sehgal <156908197+SOMYASEHGALI@users.noreply.github.com>
2024-06-18 16:49:50 +0530

    Update listing.js

--- routes/listing.js
+++ routes/listing.js
@@ -5,6 +5,10 @@ const Listing=require("../models/listing");
 const ExpressError=require("../utils/expresserror.js");
 const {listingSchema,reviewSchema}=require("../schema.js");
 const {isLoggedIn}=require("../middleware.js");
+const multer  = require('multer');
+const {storage}=require("../cloudconfig.js")
+const upload = multer({ storage })
+const listingController=require("../controllers/listings.js");
 const validateListing=(req,res,next)=>{
     let {error}=listingSchema.validate(req.body);
     if(error){
@@ -14,45 +18,53 @@ const validateListing=(req,res,next)=>{
         next();
     }
 }
+//index route
+router.get("/",wrapAsync(listingController.index));
 router.get("/",async (req,res)=>{
     const allListings= await Listing.find({});
     res.render("/index.ejs",{allListings});
     })
-    router.get("/new",isLoggedIn,(req,res)=>{
-        //console.log(req.user);
-        
-        res.render("listings/new.ejs");
-    })
+    router.get("/new",isLoggedIn,listingController.renderNewForm)
     router.put("/:id",async(req,res)=>{
         let {id}=req.params;
+        let listing=await Listing.findById(id);
+        if(!listing.owner.equals(currUser._id)){
+        req.flash("success","u not have permission to edit");
+        res.redirect(`/listings/${id}`)};
        await Listing.findByIdAndUpdate(id,{...req.body.listing});
        req.flash("success","Listing updated");
-    res.redirect("/listings");
-    })
+    res.redirect(`/listings/${id}`)
+    }),
     //show route
     router.get("/:id",async (req,res)=>{
         let {id}=req.params;
-        const listing=await Listing.findById(id).populate("reviews");
+        const listing=await Listing.findById(id).populate("reviews").populate("owner");
         if(!listing){
             req.flash("error","listing you requested does not exist");
             res.redirect("/listings");
         }
+        console.log(listing);
         res.render("listings/show.ejs",{listing});
-    });
+    }),
     //create route
-    router.post("/",
+    router.post("/",upload.single('listing[image]'),
     wrapAsync (async (req,res,next)=>{
+        let url=req.file.path;
+        let filename=req.file.filename;
+        console.log(url,"..",filename);
+        
         if(!req.body.listing){
             throw new ExpressError(400,"send valid data for listing")
         }
        // let {title,description,image,price,country,location}=req.body;
     {const newListing=new Listing(req.body.listing);
+        newListing.owner=req.user._id;
         await newListing.save();
         req.flash("success","new listing created!");
         res.redirect("/listings");}
         
        
-    }))
+    })),
     //delete route
     router.delete(
         "/:id",isLoggedIn,wrapAsync(async(req,res)=>{
@@ -61,11 +73,11 @@ router.get("/",async (req,res)=>{
             req.flash("success","listing deleted");
             res.redirect("/listings");
         })
-    )
+    ),
     //edit route
     router.get("/:id/edit",isLoggedIn,wrapAsync(async(req,res)=>{
         let {id}=req.params;
         const listing=await Listing.findById(id);
         res.render("listings/edit.ejs",{listing})
-    }))
+    })),
     module.exports=router;
