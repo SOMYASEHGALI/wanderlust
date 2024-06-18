190e08e56ef0a622e5abd188d12dcc25c397ecf5
somya sehgal <156908197+SOMYASEHGALI@users.noreply.github.com>
2024-06-18 16:57:46 +0530

    Add files via upload

---
+++ controllers/listings.js
@@ -0,0 +1,8 @@
+
+module.exports.index=async(req,res)=>{
+    const allListings=await Listing.find({});
+    res.render("listings/index.ejs",{allListings});
+}
+module.exports.renderNewForm=(req,res)=>{
+    res.render("listings/new.ejs");
+}
